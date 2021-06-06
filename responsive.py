21860998910ce783e0fbe8b482f83d694afe98af
Alex Jordan <abjordan@gmail.com>
2021-06-06 12:12:35 -0400

    Start messing around with text

--- responsive.py
+++ responsive.py
@@ -18,27 +18,31 @@ import json
 with open('config.json', 'r') as conffile:
     config = json.load(conffile)
 
-sym_font = ImageFont.truetype(config['fonts']['symbol'])
-heading_font = ImageFont.truetype(config['fonts']['bold-italic'], size=40)
+sym_font = ImageFont.truetype(config['fonts']['symbol'], size=52)
+heading_font = ImageFont.truetype(config['fonts']['bold-italic'], size=68)
+text_font = ImageFont.truetype(config['fonts']['regular'], size=52)
+bold_font = ImageFont.truetype(config['fonts']['bold'], size=52)
 
-
-bottom_third_size = (1920, 214)
+bottom_third_size = (3230, 360)
 
 img = Image.new('RGBA', bottom_third_size, color='white')
 
 logo_fs = Image.open(config['logo_file'])
 
-logo = logo_fs.resize((174, 174), Image.LANCZOS)
+logo = logo_fs.resize((280, 280), Image.LANCZOS)
 
 # Third parameter here is an Image to use as a mask. Assuming
 # the logo is saved with an alpha channel, this will respect
 # the transparency that may or may not be present.
 #img.paste(logo, (20, 20, 194, 194), logo)
-img.paste(logo, (20, 20), logo)
+img.paste(logo, (60, 40), logo)
 
 d = ImageDraw.Draw(img)
 
 d.text( (1014, 14), "Test", font=heading_font, fill='black')
+
+d.text( (550, 100), "P", font=sym_font, fill='black')
+d.text( (675, 105), "This is the day the Lord has made!", font=text_font, fill='black')
 
 img.save('test.png')
 
